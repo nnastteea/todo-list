2252d217f6966feb4a3be5fac0f55143c00de59f
nnastteea <nastyagudimchik@gmail.com>
2024-09-12 09:54:05 +0300

    convert files to json format

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    transform: {
-        '^.+\\.tsx?$': 'babel-jest'
-    },
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-};
